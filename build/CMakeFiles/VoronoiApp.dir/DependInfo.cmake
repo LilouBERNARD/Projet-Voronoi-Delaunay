c680d56d6f4b58472f8e826740c35d0003d0b0c2
LilouBERNARD <122173917+LilouBERNARD@users.noreply.github.com>
2023-06-18 23:27:57 +0200

    Add files via upload

---
+++ build/CMakeFiles/VoronoiApp.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/valer/Desktop/IMAC/Voronoi_delaunay/src/SDL2_gfxPrimitives.c" "CMakeFiles/VoronoiApp.dir/src/SDL2_gfxPrimitives.c.obj" "gcc" "CMakeFiles/VoronoiApp.dir/src/SDL2_gfxPrimitives.c.obj.d"
+  "C:/Users/valer/Desktop/IMAC/Voronoi_delaunay/src/application_ui.cpp" "CMakeFiles/VoronoiApp.dir/src/application_ui.cpp.obj" "gcc" "CMakeFiles/VoronoiApp.dir/src/application_ui.cpp.obj.d"
+  "C:/Users/valer/Desktop/IMAC/Voronoi_delaunay/src/main.cpp" "CMakeFiles/VoronoiApp.dir/src/main.cpp.obj" "gcc" "CMakeFiles/VoronoiApp.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/VoronoiApp.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "C:/Users/valer/Desktop/IMAC/Voronoi_delaunay/bin/Debug/VoronoiApp.exe"
+  "C:/Users/valer/Desktop/IMAC/Voronoi_delaunay/bin/Debug/VoronoiApp.exe.manifest"
+  "C:/Users/valer/Desktop/IMAC/Voronoi_delaunay/bin/Debug/VoronoiApp.pdb"
+  "CMakeFiles/VoronoiApp.dir/src/SDL2_gfxPrimitives.c.obj"
+  "CMakeFiles/VoronoiApp.dir/src/SDL2_gfxPrimitives.c.obj.d"
+  "CMakeFiles/VoronoiApp.dir/src/application_ui.cpp.obj"
+  "CMakeFiles/VoronoiApp.dir/src/application_ui.cpp.obj.d"
+  "CMakeFiles/VoronoiApp.dir/src/main.cpp.obj"
+  "CMakeFiles/VoronoiApp.dir/src/main.cpp.obj.d"
+  "libVoronoiApp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/VoronoiApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
